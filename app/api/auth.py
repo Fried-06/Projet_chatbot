b10b71e9769c0b851d3656e978ddfb5e0e7190c6
KOKA Essowaba David <kessowaba@gmail.com>
2026-02-10 09:06:47 +0000

    vvvvvvvv

---
+++ app/api/auth.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.bd.database import get_db
+from app.schemas.chat import UserCreate, UserLogin, UserResponse, Token
+from app.services.auth_service import register_user, login_user
+
+router = APIRouter(prefix="/api/auth")
+
+@router.post("/register", response_model=UserResponse)
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    return register_user(db, user.username, user.email, user.password)
+
+@router.post("/login", response_model=Token)
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    return login_user(db, user.email, user.password)
--- app/schemas/chat.py
+++ app/schemas/chat.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 
 
 class ChatRequest(BaseModel):
@@ -12,3 +12,30 @@ class ChatRequest(BaseModel):
 
 class ChatResponse(BaseModel):
     response: str   
+
+class UserCreate(BaseModel):
+    usermane: str = Field(..., min_length=3, max_length=50, description="The user's username, must be between 3 and 50 characters long.")
+    nom : str = Field(..., min_length=1, max_length=100, description="The user's full name.")
+    prenom : str = Field(..., min_length=1, max_length=100, description="The user's full name.")
+    email: EmailStr
+    password: str = Field(..., min_length=6, description="The user's password, must be at least 6 characters long.")
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str = Field(..., min_length=6, description="The user's password, must be at least 6 characters long.")
+
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    nom : str
+    prenom : str
+
+    class Config:
+        form_attributes = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str 
